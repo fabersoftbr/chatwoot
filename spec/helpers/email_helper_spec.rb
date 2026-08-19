a71a479927306b78a19a9e7089203ef0a385c5a9
Pedro Santos Kajiya <kajiyapedro@gmail.com>
2026-08-19 20:11:01 -0300

    test(email_helper): cover generic email domain predicate

--- spec/helpers/email_helper_spec.rb
+++ spec/helpers/email_helper_spec.rb
@@ -16,4 +16,32 @@ describe EmailHelper do
       end
     end
   end
+
+  describe '#generic_email_domain?' do
+    it 'returns true for free mailbox providers' do
+      expect(helper.generic_email_domain?('john@gmail.com')).to be(true)
+      expect(helper.generic_email_domain?('john@hotmail.com.br')).to be(true)
+      expect(helper.generic_email_domain?('john@proton.me')).to be(true)
+    end
+
+    it 'returns false for company domains' do
+      expect(helper.generic_email_domain?('john@acme.com')).to be(false)
+      expect(helper.generic_email_domain?('john@stripe.com')).to be(false)
+    end
+
+    it 'ignores case and surrounding whitespace' do
+      expect(helper.generic_email_domain?('john@GMAIL.com')).to be(true)
+      expect(helper.generic_email_domain?('john@Hotmail.com.BR ')).to be(true)
+    end
+
+    it 'does not match a company domain that merely ends with a provider domain' do
+      expect(helper.generic_email_domain?('john@notgmail.com')).to be(false)
+      expect(helper.generic_email_domain?('john@mail.gmail.com')).to be(false)
+    end
+
+    it 'returns false when the address has no domain' do
+      expect(helper.generic_email_domain?('')).to be(false)
+      expect(helper.generic_email_domain?(nil)).to be(false)
+    end
+  end
 end
